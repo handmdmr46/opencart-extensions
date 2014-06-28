a35a9a1609ee1f05eb5d8b0e13268af20de9d7a8
Martin Hand <martin@mdhmotors.com>
2014-06-28 15:24:02 -0700

    added basic controller and view code for starting point

--- admin/controller/affiliate/stock_control.php
+++ admin/controller/affiliate/stock_control.php
@@ -1,5 +1,90 @@
 <?php
 class ControllerAffiliateStockControl extends Controller {
+	public function index() {
+		$this->language->load('affiliate/stock_control');
+		$this->document->setTitle($this->language->get('heading_title_stock_control'));
+		$this->load->model('affiliate/csv_import');
+		$this->load->model('affiliate/stock_control');
+		
+		// addScript and addStyle examples
+		/*
+		$this->document->addScript('view/javascript/event_scheduler/codebase/dhtmlxscheduler.js');
+    	$this->document->addScript('view/javascript/event_scheduler/codebase/ext/dhtmlxscheduler_year_view.js');
+    	$this->document->addStyle('view/javascript/event_scheduler/codebase/dhtmlxscheduler.css');
+    	*/
+
+		$this->init();
+
+	}
+
+	protected function init() {
+		// Breadcrumbs
+	    $this->data['breadcrumbs'] = array();
+
+		$this->data['breadcrumbs'][] = array(
+       		'text'      => $this->language->get('text_home'),
+       		'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+       		'separator' => false
+		);
+
+		$this->data['breadcrumbs'][] = array(
+       		'text'      => $this->language->get('heading_title_stock_control'),
+       		'href'      => $this->url->link('affiliate/stock_control', 'token=' . $this->session->data['token'], 'SSL'),
+       		'separator' => ' :: '
+		);
+
+		// Language
+		$this->data['heading_title']                   = $this->language->get('heading_title_stock_control');
+		// $this->data['']                   = $this->language->get('');
+
+		// Error
+	    if (isset($this->session->data['error'])) {
+	      $this->data['error'] = $this->session->data['error'];
+	      unset($this->session->data['error']);
+	    } else {
+	      $this->data['error'] = '';
+	    }
+
+	    // Success
+	    if (isset($this->session->data['success'])) {
+	      $this->data['success'] = $this->session->data['success'];
+	      unset($this->session->data['success']);
+	    } else {
+	      $this->data['success'] = '';
+	    }
+
+	    // Page, Start & Limit -- pagination --
+	    /*if (isset($this->request->get['page'])) {
+	      $page = $this->request->get['page'];
+	      $this->data['page'] = $this->request->get['page'];
+	    } else {
+	      $page = 1;
+	      $this->data['page'] = 1;
+	    }
+
+	    $url = '';
+
+	    if (isset($this->request->get['page'])) {
+	      $url .= '&page=' . $this->request->get['page'];
+	    }
+
+	    $limit = 100;
+	    $start = ($page - 1) * $limit;*/
+
+		// Buttons
+	    $this->data['import_ids'] = $this->url->link('affiliate/ebayid_import/importIds', 'token=' . $this->session->data['token'] . $url, 'SSL');
+
+		// Load Template for View
+		$this->template = 'affiliate/stock_control.tpl';
+
+	    $this->children = array(
+	      'common/header',
+	      'common/footer'
+	    );
+
+	    $this->response->setOutput($this->render());
+
+	}
 
 
 }
--- admin/view/template/affiliate/stock_control.tpl
+++ admin/view/template/affiliate/stock_control.tpl
@@ -1,6 +1,76 @@
 <?php echo $header; ?>
-<div id="container">
+
 <div id="content">
+
+	<div class="breadcrumb">
+	  <?php foreach ($breadcrumbs as $breadcrumb) { ?>
+	    <?php echo $breadcrumb['separator']; ?><a href="<?php echo $breadcrumb['href']; ?>"><?php echo $breadcrumb['text']; ?></a>
+	  <?php } ?>
+    </div>
+
+    <?php if ($error) { ?>
+      <div class="warning"><?php echo $error; ?></div>
+    <?php } ?>
+
+    <?php if ($success) { ?>
+      <div class="success"><?php echo $success; ?></div>
+    <?php } ?>
+
+    <div class="box">
+        <div class="heading">
+            <h1><img src="view/image/download.png" alt="" /> <?php echo $heading_title; ?></h1>
+            <h1 class="wait" style="margin-left:1700px; display: none;">Please Wait, this may take awhile..... &nbsp;<img src="view/image/loading.gif" alt="" width="20" height="20" /></h1>
+            <div class="buttons">
+              <a onclick="start_ebay_call(); $('#form').attr('action', '<?php echo $import_ids; ?>'); $('#form').submit();" class="button"><?php echo $button_import; ?></a>
+              <a onclick="$('#form').attr('action', '<?php echo $load_profile; ?>'); $('#form').submit();" class="button"><?php echo $button_load_profile; ?></a>
+              <a onclick="$('#form').attr('action', '<?php echo $update_profile; ?>'); $('#form').submit();" class="button"><?php echo $button_update_profile; ?></a>
+              <a href="<?php echo $cancel; ?>" class="button"><?php echo $button_cancel; ?></a>
+            </div>
+        </div><!-- .heading -->
+
+        <div class="content">
+        	<form action="" method="post" enctype="multipart/form-data" id="form">
+        	<table class="form">
+        	</table>
+        	</form>
+        </div><!-- .content -->
+
+    </div><!-- .box -->
+
+	
+
 </div><!-- #content -->
-</div><!-- #container -->
+
+<script type="text/javascript"><!--
+
+  function start_ebay_call(){
+    $('.wait').show();
+    // dont need this timer because resets when page re-loads.
+    // setTimeout(function(){$('.wait').hide();}, 999000);
+  }
+
+  function check(id) {
+    document.getElementById(id + '_select').setAttribute('checked','checked');
+  }
+
+//--></script>
+
+<script type="text/javascript" charset="utf-8">
+
+  window.onload=function(){
+    scheduler.config.xml_date="%Y-%m-%d";
+    scheduler.config.year_x = 6; //2 months in a row
+    scheduler.config.year_y = 2; //3 months in a column
+    scheduler.init('scheduler_here', new Date(), 'year');
+    scheduler.parse('<?php echo $dates; ?>', 'json');
+  };
+
+</script>
+
+<script type="text/javascript"><!--
+
+  $('#tabs a').tabs();
+
+//--></script>
+
 <?php echo $footer; ?>
